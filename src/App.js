107b52f707e50b5549b65f8a593880b36a9d155b
TRZMlEL <mateusztrzmiel2104@gmail.com>
2023-08-23 16:21:51 +0200

    modified:   src/App.js 	new file:   src/img/code.png

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import mateuszTrzmiel from './img/MateuszTrzmiel.png';
 import separator from './img/Separator.png';
+import code from './img/code.png';
 
 // pamiętaj robić npm run deploy
 
@@ -29,14 +30,21 @@ function App() {
         <div className='w-1/2 h-5/6  flex items-center justify-center'>
           <img alt="Matuesz Trzmiel Photo" src={mateuszTrzmiel} className='w-mojezdjecie'/>
         </div>
-        <img alt="Matuesz Trzmiel Photo" src={separator} className='w-full absolute -bottom-28'/>
+        <img alt="" src={separator} className='w-full absolute -bottom-28'/>
       </section>
-        <section className='bg-primary' >
-          <p className='text-3xl text-secondary'>
-          Hi, <s>👋</s>I've been learning programming for half a year to become
+        <section className='bg-primary flex flex-wrap pl-webborder pr-webborder' >
+          <img alt="Code" src={code} className='w-1/2 pr-webborder mb-webborder rounded-3xl object-cover'/>
+          <p className='text-3xl text-secondary w-1/2 pl-webborder'>
+          Hi, 👋I've been learning programming for half a year to become
            a frontend developer, in the meantime, since programming, I also deal with photography.<br />
           <a>See my work</a>
           </p>
+        </section>
+        <section>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
         </section>
       </main>
       <footer></footer>
