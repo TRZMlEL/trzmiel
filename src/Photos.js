b3137989b3ea680b2350a8562ae8bf7888218c6a
TRZMlEL <mateusztrzmiel2104@gmail.com>
2023-08-30 20:01:42 +0200

    modified:   src/Photos.js

--- src/Photos.js
+++ src/Photos.js
@@ -1,24 +1,29 @@
-import React from 'react'
-import Building from './img/_TRZ9137-Poprawione-Szum.jpg'
-import Start from './img/Gwiazdy.jpg'
-import WildTrain from './img/_TRZ8462.jpg'
+import React, { useRef, useEffect } from 'react';
+import Building from './img/_TRZ9137-Poprawione-Szum.jpg';
+import Stars from './img/Gwiazdy.jpg';
+import WildTran from './img/_TRZ8462.jpg';
 
-function photos() {
+function Photos() {
+
+
   return (
     <div className='bg-color w-screen mt-6rem flex items-center justify-center pt-4rem flex-col'>
         <div className='w-full mb-2rem flex items-center justify-center'>
             <h2 className='font-semibold text-3xl'>PHOTOS</h2>
         </div>
-        <div className='h-25rem flex items-center justify-center gap-1rem md-4rem'>
-            <img className='photo' alt='Building with balkons during golden hour' src={ Building } />
-            <img className='photo' alt='Long exposition of night sky' src={ Start } />
-            <img className='photo' alt='Train in wild forest' src={ WildTrain } />
-            <img className='photo' alt='Building with balkons during golden hour' src={ Building } />
-            <img className='photo' alt='Long exposition of night sky' src={ Start } />
-            <img className='photo' alt='Train in wild forest' src={ WildTrain } />
+        <div className='h-auto flex flex-wrap justify-center gap-1rem mb-4rem'>
+          <img className="h-20rem w-27rem object-cover" src={ Building } draggable="false" alt="Building" />
+          <img className="h-20rem w-27rem object-cover" src={ Stars } draggable="false" alt="Stars" />
+          <img className="h-20rem w-27rem object-cover" src={ WildTran } draggable="false" alt="WildTran" />
+          <img className="h-20rem w-27rem object-cover" src={ Building } draggable="false" alt="Building" />
+          <img className="h-20rem w-27rem object-cover" src={ Stars } draggable="false" alt="Stars" />
+          <img className="h-20rem w-27rem object-cover" src={ WildTran } draggable="false" alt="WildTran" />
+          <img className="h-20rem w-27rem object-cover" src={ Building } draggable="false" alt="Building" />
+          <img className="h-20rem w-27rem object-cover" src={ Stars } draggable="false" alt="Stars" />
+          <img className="h-20rem w-27rem object-cover" src={ WildTran } draggable="false" alt="WildTran" />
         </div>
     </div>
-  )
+  );
 }
 
-export default photos
+export default Photos;
